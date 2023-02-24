2efe7865f05bdf48ae32fb60d971f6a63b2803a1
Jesvin Joseph <Jesvinkjoseph24@gmail.com>
2023-02-24 17:56:16 -0500

    lab6done

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -2,6 +2,21 @@
 
 int search(int numbers[], int low, int high, int value) 
 {
+	if (low <= high) {
+		int mid = (low + high) / 2;
+
+		if (value < numbers[mid])
+		{
+			return search(numbers, low, mid - 1, value);
+		}
+		else if (value > numbers[mid])
+		{
+			return search(numbers, mid + 1, high, value);
+		}
+		else {
+			return mid;
+		}
+	}
 	return -1;
 }
 
